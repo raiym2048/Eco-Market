621f6366aabff52b918a505c1dc090830be9ad98
Unity01 <76080041+raiym2048@users.noreply.github.com>
2024-03-15 15:33:51 +0600

    Initial commit

---
+++ src/main/java/kg/itsphere/eco_market/Eco/Market/EcoMarketApplication.java
@@ -0,0 +1,13 @@
+package kg.itsphere.eco_market.Eco.Market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kg/itsphere/eco_market/Eco/Market/EcoMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.itsphere.eco_market.Eco.Market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
