cc2effbe3a3e963054fba68462e3c999d6049b25
Nurmukhanbet <nurmuhanbet2004@icloud.com>
2024-03-16 14:57:32 +0600

    Admin's function with product is done
    Additonally expceptions also added

--- src/main/java/kg/itsphere/eco_market/Eco/Market/service/ProductService.java
+++ src/main/java/kg/itsphere/eco_market/Eco/Market/service/ProductService.java
@@ -1,4 +1,9 @@
 package kg.itsphere.eco_market.Eco.Market.service;
 
+import kg.itsphere.eco_market.Eco.Market.web.dto.product.ProductRequest;
+
 public interface ProductService {
+    void add(ProductRequest productRequest);
+    void updateByName(String name, ProductRequest productRequest);
+    void deleteByName(String name);
 }
--- src/main/java/kg/itsphere/eco_market/Eco/Market/web/dto/product/ProductResponse.java
+++ src/main/java/kg/itsphere/eco_market/Eco/Market/web/dto/product/ProductResponse.java
@@ -1,9 +1,16 @@
 package kg.itsphere.eco_market.Eco.Market.web.dto.product;
 
+import kg.itsphere.eco_market.Eco.Market.domain.entity.product.Category;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 @Data
 public class ProductResponse {
+    private Long id;
+    private String name;
+    private String description;
+    private Category category;
+    private int price;
+    private int quantity;
 }
