eada3e5a9b0a12303aa74bc768d9819103a1ae39
Erdan <asangulovtimur6@gmail.com>
2024-03-27 12:10:16 +0100

    verify fixed

--- src/main/java/kg/itsphere/eco_market/Eco/Market/service/AuthService.java
+++ src/main/java/kg/itsphere/eco_market/Eco/Market/service/AuthService.java
@@ -5,12 +5,14 @@ import jakarta.servlet.http.HttpServletResponse;
 import kg.itsphere.eco_market.Eco.Market.domain.entity.user.User;
 import kg.itsphere.eco_market.Eco.Market.web.dto.auth.AuthLoginRequest;
 import kg.itsphere.eco_market.Eco.Market.web.dto.auth.AuthLoginResponse;
+import kg.itsphere.eco_market.Eco.Market.web.dto.user.CodeRequest;
 import kg.itsphere.eco_market.Eco.Market.web.dto.user.UserRegisterRequest;
 
 import java.io.IOException;
 
 public interface AuthService {
     void register(UserRegisterRequest userRegisterRequest);
+
     AuthLoginResponse login(AuthLoginRequest authLoginRequest);
     public User getUserFromToken(String token);
 
