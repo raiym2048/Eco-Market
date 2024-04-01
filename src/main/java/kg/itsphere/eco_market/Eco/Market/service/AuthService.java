847be9d686b5cf7af0aea34688d4f3430238545d
Erdan <asangulovtimur6@gmail.com>
2024-04-01 12:52:20 +0200

    deleted TokenRepository and token entities

--- src/main/java/kg/itsphere/eco_market/Eco/Market/service/AuthService.java
+++ src/main/java/kg/itsphere/eco_market/Eco/Market/service/AuthService.java
@@ -1,14 +1,11 @@
 package kg.itsphere.eco_market.Eco.Market.service;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import kg.itsphere.eco_market.Eco.Market.domain.entity.user.User;
 import kg.itsphere.eco_market.Eco.Market.web.dto.auth.AuthLoginRequest;
 import kg.itsphere.eco_market.Eco.Market.web.dto.auth.AuthLoginResponse;
-import kg.itsphere.eco_market.Eco.Market.web.dto.user.CodeRequest;
+
 import kg.itsphere.eco_market.Eco.Market.web.dto.user.UserRegisterRequest;
 
-import java.io.IOException;
 
 public interface AuthService {
     void register(UserRegisterRequest userRegisterRequest);
